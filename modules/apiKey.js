907960536c6eed9797605a74c39f8af0ac32e462
Nikita <np_v@inbox.ru>
2025-02-11 12:23:26 +0300

    create app

---
+++ modules/apiKey.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const api_key = process.env.API_KEY;
+const client_id = process.env.CLIENT_ID;
+
+const headers = { "Client-Id": client_id, "Api-Key": api_key }
+const api_links = {
+    "SaleList": {
+        // # Список доступных акций
+        "normal": "https://api-seller.ozon.ru/v1/actions"
+    },
+    "SaleCandidates": {
+        // # Список доступных для акции товаров
+        "normal": "https://api-seller.ozon.ru/v1/actions/candidates"
+    },
+    "ProductsInSale": {
+        // # Список доступных для акции товаров
+        "normal": "https://api-seller.ozon.ru/v1/actions/products"
+    },    
+    "AddToSale": {
+        // # Добавить товар в акцию
+        "normal": "https://api-seller.ozon.ru/v1/actions/products/activate"
+    },
+    "RemoveFromSale": {
+        // # Удалить товары из акции
+        "normal": "https://api-seller.ozon.ru/v1/actions/products/deactivate"
+    }, 
+    // # Список товаров
+    "ProductList": "https://api-seller.ozon.ru/v3/product/list",
+}
+
+export { api_key, client_id, headers, api_links };
---
+++ script.js
@@ -0,0 +1,5 @@
+import { api_key, client_id, headers, api_links } from './modules/apiKey.js'
+
+function getSaleFile() {
+    
+}
